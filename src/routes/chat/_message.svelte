b8144267e989a4684b87333994d7bf17e1ab7aa6
Evert van Brussel <ecvanbrussel@gmail.com>
2020-07-13 02:36:17 +0200

    shuffle colors

--- src/routes/chat/_message.svelte
+++ src/routes/chat/_message.svelte
@@ -16,12 +16,12 @@
   const colors = [
     'text-red-500',
     'text-green-500',
-    'text-orange-500',
-    'text-blue-500',
-    'text-yellow-500',
     'text-purple-500',
+    'text-yellow-500',
     'text-teal-500',
     'text-pink-500',
+    'text-blue-500',
+    'text-orange-500',
     'text-indigo-500',
   ]
 
